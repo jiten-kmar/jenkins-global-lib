21982d40fc86ad2fedd8e30be762252a54aa3a8c
jitender1278 <jitender1278>
2021-04-09 15:01:25 -0400

    first commit for template

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -1,9 +1,10 @@
 def call(Map args) {
-	app = docker.build("${args.remote}/${args.project}/${args.imageName}:${args.tag}", "${args.otherargs} --file ${args.filePath} .")
-	docker.withRegistry("${GCR_URL}", "${GOOGLE_PROJECT_CRED_ID}") {
+	app = docker.build("${args.remote}/${args.project}/${args.imageName}:${args.tag}", "${args.otherargs} --file ${args.filePath} ${args.contextPath}")
+	echo "Image tagged"
+	docker.withRegistry("https://${GCR_URL}", "gcr:${GOOGLE_PROJECT_CRED_ID}") {
 		app.push()
 		app.push("latest")
 	}
 	sh "docker rmi ${args.remote}/${args.project}/${args.imageName}:${args.tag}"
-	echo "Finished creation of docker images and pushed to GCR" 
+	echo "Finished creation of docker images and pushed to GCR"
 }
